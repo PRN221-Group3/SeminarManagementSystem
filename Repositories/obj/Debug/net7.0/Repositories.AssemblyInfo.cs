52d929d84754ecde5b19250012301bfb28c5c460
ZienMinh <MinhNLVSE172956@fpt.edu.vn>
2024-06-10 20:45:24 +0700

    Remove services

--- Repositories/obj/Debug/net7.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net7.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa8682ce32ce3667ee5816b5e0811a5d2f4b381e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c55a10a0ffcd1d356f8d535c6b7d8a4a61a1436a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
