20482a7dce724c7aba5bf8e5418fb5ae662bafcd
nam127 <buithanhnam9109@gmail.com>
2024-06-11 18:08:15 +0700

    Feature: forgot password

---
+++ Services/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IUserService
+    {
+        //public List<User> GetAll();
+        //public User GetById(Guid id);
+        //public void Add(User user);
+        public User GetByEmail(string email);
+        public void Update(User user);
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using BusinessObject.Models;
+using Repositories.Interfaces;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _repository;
+        public UserService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public User GetByEmail(string email)
+        {
+            return _repository.GetByEmail(email);
+        }
+
+        public void Update(User user)
+        {
+            _repository.Update(user);
+        }
+    }
+}
