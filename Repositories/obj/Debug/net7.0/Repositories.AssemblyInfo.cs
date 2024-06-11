c919c3321c4717dc41463c7ae378dbe99ad0eef0
nam127 <buithanhnam9109@gmail.com>
2024-06-11 23:12:26 +0700

    init

--- Repositories/obj/Debug/net7.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net7.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4112fef2a586d63e36cfab6608854089f1f833c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
