daa227b53cde1751459c5c926fd40ef52fbf2afa
nam127 <buithanhnam9109@gmail.com>
2024-06-12 10:18:34 +0700

    clean proj

---
+++ SeminarManagement_PRN221/Pages/VerifyToken.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SeminarManagement_PRN221.Pages
+{
+    public class VerifyTokenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
