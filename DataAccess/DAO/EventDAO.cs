573bf3136f870eaa22e8acb63b9a87a31a72a120
nam127 <buithanhnam9109@gmail.com>
2024-06-18 00:13:03 +0700

    view events sponsored

--- DataAccess/DAO/EventDAO.cs
+++ DataAccess/DAO/EventDAO.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using BusinessObject.Models;
 using DataAccess.DAO.Base;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.DAO
 {
@@ -12,6 +13,22 @@ namespace DataAccess.DAO
     {
         public EventDAO(SeminarManagementDbContext context) : base(context)
         {
+        }
+
+        public IEnumerable<Event> GetEventsSponsored(Guid sponsorId)
+        {
+            var eventsSponsor = from e in _context.Events
+                                join es in _context.EventSponsors
+                                on e.EventId equals es.EventId
+                                where es.SponsorId == sponsorId && es.Status.Equals("Accepted") && e.IsDeleted == false
+                                select e;
+            
+            return eventsSponsor.ToList();
+        }
+
+        public async Task<EventSponsor?> GetEventSponsor(Guid sponsorId)
+        {
+            return await _context.EventSponsors.FirstOrDefaultAsync(x => x.SponsorId == sponsorId);
         }
     }
 }
--- SeminarManagement_PRN221/Pages/UserRole/SponsorWorkspace.cshtml.cs
+++ SeminarManagement_PRN221/Pages/UserRole/SponsorWorkspace.cshtml.cs
@@ -1,12 +1,29 @@
+using BusinessObject.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repositories.Interfaces;
+using System.Security.Claims;
 
 namespace SeminarManagement_PRN221.Pages.UserRole.SponsorWorkspace;
 
 [Authorize(Roles = "Sponsor")]
 public class IndexModel : PageModel
 {
-    public void OnGet()
+    private readonly IEventRepository _eventRepository;
+    public IndexModel(IEventRepository eventRepository)
     {
+        _eventRepository = eventRepository;
+    }
+    [BindProperty]
+    public IEnumerable<Event> Events { get; set; }
+    [BindProperty]
+    public EventSponsor EventSponsor {  get; set; }
+    public async Task<IActionResult> OnGet()
+    {
+        var userId = Guid.Parse(HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+        Events = await _eventRepository.GetEventsSponsored(userId);
+        EventSponsor = await _eventRepository.GetEventSponsor(userId);
+        return Page();
     }
 }
