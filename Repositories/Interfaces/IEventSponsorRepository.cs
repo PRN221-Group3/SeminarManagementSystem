16766dda53fc121bb451afe6a07eb256bb43c604
kieutranquocson5 <kieutranquocson5@gmail.com>
2024-06-21 23:02:40 +0700

    fix bugs (hide password,fix EventSponsorRepo

--- Repositories/Interfaces/IEventSponsorRepository.cs
+++ Repositories/Interfaces/IEventSponsorRepository.cs
@@ -3,14 +3,13 @@
 using System.Linq;
 using System.Threading.Tasks;
 using BusinessObject.Models;
+using Repositories.BaseRepo;
 
 namespace Repositories.Interfaces
 {
-    public interface IEventSponsorRepository
+    public interface IEventSponsorRepository : IBaseRepository<EventSponsor>
     {
         public Task<EventSponsor?> GetEventSponsor(Guid sponsorId, Guid eventId);
-        Task<IQueryable<EventSponsor>> GetAllQueryableAsync();
-        Task AddAsync(EventSponsor eventSponsor);
         Task<IEnumerable<EventSponsor>> GetByEventIdAsync(Guid eventId);
     }
 }
